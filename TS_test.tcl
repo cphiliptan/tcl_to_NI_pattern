743d25b74880a6b5f11c0750f7758a848d73a840
cphiliptan <cphiliptan81@gmail.com>
2024-01-08 15:53:30 +0800

    2nd commit

---
+++ TS_test.tcl
@@ -0,0 +1,25 @@
+#Tune the clock to 80MHz
+w 7f 00; # page 0x0
+w 04 04; # tune the OSC clock
+w 07 xx; # depending the freq setting
+
+
+#Temperature Measurement
+w 7f 00;
+w 50 03; # start temp sensor sequence
+w 50 00; # stop temp sensor sequence
+
+#Selection 1
+w 7f 00; # page 0x0
+w 17 40; # selection 1
+
+w 2f 00; # smaller cap for IPTAT, ICTAT
+w 32 40; # bigger cap INOM 
+
+Selection 0
+w 7f 00; # page 0x0
+w 17 00; # selection 0
+
+w 2f 03; # bigger cap for IPTAT, ICTAT
+w 32 40; # bigger cap INOM
+
--- main.py
+++ main.py
@@ -3,14 +3,45 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
+import pandas as pd
+import re
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def open_file(file_name):
+    print(f'Opening File: {file_name}')
+
+    file1 = open(file_name, 'r')
+    all_lines = file1.readlines()
+
+    count = 0
+
+    line_list = []
+    # Strips the newline character
+    for line in all_lines:
+        count += 1
+        line = line.strip()
+
+        # pattern = r"^[wW]"
+        # pattern = r"^[wW]"
+        pattern = r"^[wW]\s+(\w+)\s+(\w+);"
+
+        match = re.search(pattern, line)
+        if match:
+            line_list.append(line)
+            print(line)
+
+            arg1 = match.group(1)
+            arg2 = match.group(2)
+            print(f"1st argument: {arg1}, 2nd argument: {arg2}")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    open_file('TS_test.tcl')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
