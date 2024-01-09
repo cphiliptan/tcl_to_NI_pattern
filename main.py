f9da4e920106037503bbb66ef431f4b2870d503e
cphiliptan <cphiliptan81@gmail.com>
2024-01-09 10:54:27 +0800

    6th commit

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def open_file(file_name):
         match = re.search(pattern, line)
         if match:
             line_list.append(line)
-            print(line)
+            print("//" + line)
 
             arg1 = int(match.group(1), 16)
             arg2 = int(match.group(2), 16)
@@ -40,54 +40,56 @@ def open_file(file_name):
             cmd_add_data_list.append(["w", arg1, arg2])
 
 
+def write_spi(add, m_data):
+    # write function
+    out_string = ""
+    ncs = 0
+    sclk = 0
+    mosi = 0
+    miso = "X"
+    print(f"// write to address: 0x{add:02x} | 0x80")
+    wr_address = add | 0x80
+    for bit in range(7, -1, -1):  # 7 to 0
+        mosi = (wr_address & pow(2, bit)) >> bit
+        # NCS SCLK MOSI MISO
+        out_string = ("tset_SPI" + "\t" +
+                      str(ncs) + "\t" +
+                      str(sclk) + "\t" +
+                      str(mosi) + "\t" +
+                      str(miso) + ";")
+        print(out_string)
+    sclk = 1
+    out_string = ("tRead" + "\t\t" +
+                  str(ncs) + "\t" +
+                  str(sclk) + "\t" +
+                  str(mosi) + "\t" +
+                  str(miso) + ";")
+    print(out_string)
+    sclk = 0
+    print(f"// write data: 0x{m_data:02x} ")
+    for bit in range(7, -1, -1):  # 7 to 0
+        mosi = m_data & pow(2, bit) >> bit
+        # NCS SCLK MOSI MISO
+        out_string = ("tset_SPI" + "\t" +
+                      str(ncs) + "\t" +
+                      str(sclk) + "\t" +
+                      str(mosi) + "\t" +
+                      str(miso) + ";")
+        print(out_string)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     open_file('TS_test.tcl')
-    print(*cmd_add_data_list, sep="\n")
+    # print(*cmd_add_data_list, sep="\n")
 
     action = cmd_add_data_list[0][0]
     address = cmd_add_data_list[0][1]
     data = cmd_add_data_list[0][2]
 
-    # write function
-    out_string = ""
-    NCS  = 0
-    SCLK = 0
-    MOSI = 0
-    MISO = "X"
-    print(f"// write to address: 0x{address:02x} | 0x80")
-    address = address | 0x80
-    for bit in range(7, -1, -1):  # 7 to 0
-        MOSI = address & pow(2, bit) >> bit
-        # NCS SCLK MOSI MISO
-        out_string = ("tset_SPI" + "\t" +
-                      str(NCS) + "\t" +
-                      str(SCLK) + "\t" +
-                      str(MOSI) + "\t" +
-                      str(MISO) + ";")
-        print(out_string)
-
-    SCLK = 1
-    out_string = ("tRead" + "\t\t" +
-                  str(NCS) + "\t" +
-                  str(SCLK) + "\t" +
-                  str(MOSI) + "\t" +
-                  str(MISO) + ";")
-    print(out_string)
-
-    SCLK = 0
-    print(f"// write data: 0x{data:02x} ")
-    for bit in range(7, -1, -1):  # 7 to 0
-        MOSI = data & pow(2, bit) >> bit
-        # NCS SCLK MOSI MISO
-        out_string = ("tset_SPI" + "\t" +
-                      str(NCS) + "\t" +
-                      str(SCLK) + "\t" +
-                      str(MOSI) + "\t" +
-                      str(MISO) + ";")
-        print(out_string)
-
-
+    for cmd in cmd_add_data_list:
+        if cmd[0] == 'w' or cmd[0] == 'W':
+            write_spi(cmd[1], cmd[2])
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
