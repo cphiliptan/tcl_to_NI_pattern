3f316dec3e4d849ba5537d6482c18029b672e990
cphiliptan <cphiliptan81@gmail.com>
2024-01-11 17:03:30 +0800

    21th commit

--- debug.tcl
+++ debug.tcl
@@ -1,5 +1,20 @@
-w 7f 00; # write 0x7f 0x00
-r 7f 00; # read 0x7f 0x00
-r 08 48; # SPI_CTL5 Default 0x48
-w 08 4b; # SPI_CTL5
-r 08 4b; # SPI_CTL5 readback after write
+w 3a 5a; # soft reset
+wait 10;
+w 7f 00; # read page #
+r 7f 00; # read page #
+r 00 f1; # read Product ID
+r 03 03; # SPI_CTL0
+
+w 7f 01; # read page #
+r 7f 01; # read page #
+wait 10;
+r 00 f1; # read Product ID
+r 03 1a; # ADC_CTL00
+
+w 7f 02; # read page #
+r 7f 02; # read page #
+wait 10;
+r 00 f1; # read Product ID
+r 10 fc; # 0x210
+r 11 3f; # 0x211
+
